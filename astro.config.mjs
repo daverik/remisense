e52946ec2fa2af63bf6e785134bdc868c526a371
David Eriksson <eriksson.o.david@gmail.com>
2025-05-28 22:07:23 +0200

    set base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/remisense/",
   outDir: "./docs",
   vite: {
     plugins: [tailwindcss()],
